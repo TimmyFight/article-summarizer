0ae2409b8acc3f4e55430c02dbc9a98110b8db38
Adrian Ciolkiewicz <adrian.ciolkiewicz@makimo.pl>
2023-10-10 16:22:44 +0200

    feat: Render information when article was not found, and update tests

--- src/Templates/SummarizerTemplate/SummarizerTemplate.test.tsx
+++ src/Templates/SummarizerTemplate/SummarizerTemplate.test.tsx
@@ -4,14 +4,16 @@ import { describe, expect, it } from "vitest";
 import SummarizerTemplate from "./SummarizerTemplate";
 
 describe("SummarizerTemplate has been rendered", () => {
-  it("The container div is rendered", () => {
+  it("The container div is rendered without article", () => {
     render(<SummarizerTemplate />);
     const container = screen.getByTestId("summarizerContainer");
-    expect(container).toBeTruthy();
+    expect(container.textContent).toBe(
+      "Article was not found. Try to search by URL."
+    );
   });
   it("The container div is rendered with article", () => {
     render(<SummarizerTemplate article="Example article" />);
     const container = screen.getByTestId("summarizerContainer");
-    expect(container.textContent).toMatch("Example article");
+    expect(container.textContent).toBe("Example article");
   });
 });
--- src/Templates/SummarizerTemplate/SummarizerTemplate.tsx
+++ src/Templates/SummarizerTemplate/SummarizerTemplate.tsx
@@ -2,9 +2,16 @@ interface SummarizerProps {
   article?: string;
 }
 
-const SummarizerTemplate = ({ article }: SummarizerProps) => (
-  <div data-testid="summarizerContainer">{article}</div>
-);
+const SummarizerTemplate = ({ article }: SummarizerProps) => {
+  if (article === "Empty") {
+    return (
+      <div data-testid="summarizerContainer">
+        Article was not found. Try to search by URL.
+      </div>
+    );
+  }
+  return <div data-testid="summarizerContainer">{article}</div>;
+};
 
 SummarizerTemplate.defaultProps = {
   article: "Empty",

b3de4dd84811d6466f3cdfa2172fbc852c780a1b
Adrian <adrian.ciolkiewicz@makimo.pl>
2023-10-30 08:49:36 +0100

    fix: Use req in handler mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,6 +6,7 @@ export const handlers = [
   rest.get(
     "https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=https://wheaterapp-adi.netlify.app&length=3",
     (req, res, ctx) => {
+      console.log(req);
       return res(ctx.delay(1000), ctx.json({ summary: "Test summary" }));
     }
   ),
